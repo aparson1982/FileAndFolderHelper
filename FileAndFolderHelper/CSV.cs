8fa77eb2e2a1fda761cc1ee7769f6a194cbd977c
Parson, Robert <wrb176@mocs.utc.edu>
2020-02-12 11:39:24 -0500

    Add project files.

---
+++ FileAndFolderHelper/CSV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndFolderHelper
+{
+    public class CSV
+    {
+        public static int GetLengthFromCSV(string Path, string delimiter)
+        {
+            char delimit = delimiter[0];
+            List<string> values = File.ReadAllLines(Path).Select(v => ReadFromCSV(v, delimit).ToString()).ToList();
+            return values.Count;
+            
+        }
+        internal static string[] ReadFromCSV(string CsvLine, char delimiter)
+        {
+            string[] values = CsvLine.Split(delimiter);
+            return values;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileAndFolderHelper;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(CSV.GetLengthFromCSV(@"\\nas72v2\vdi_data\USA\rparso2\Documents\Automation Anywhere Files\Automation Anywhere\Shaw Files\0066_InvoiceCDPackages\test.csv",","));
+        }
+    }
+}
